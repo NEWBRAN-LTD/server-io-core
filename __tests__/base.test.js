4f5bb866b3a0a9be4d98877ede817c026db16953
Joel Chu <joelckchu@gmail.com>
2018-09-08 16:51:38 +0800

    Fix the test

--- __tests__/base.test.js
+++ __tests__/base.test.js
@@ -2,27 +2,29 @@
  * Base test to see if the server start up without any options
  */
 const test = require('ava');
-// const request = require('superkoa');
-// const serverIoCore = require('../index');
-// const { join } = require('path');
-/*
-test.beforeEach(t => {
-  const w = serverIoCore({
-    autoStart: false,
+const request = require('superkoa');
+const serverIoCore = require('../index');
+const { join } = require('path');
+
+test.before(t => {
+  const { app, stop } = serverIoCore({
+    // autoStart: false,
+    debugger: false,
+    reload: false,
     webroot: join(__dirname, 'fixtures', 'demo', 'dist', 'base')
   });
-  t.context.app = w.app;
-  t.context.stop = w.stop;
+  t.context.app = app;
+  t.context.stop = stop;
 });
 
-test.afterEach(t => {
-  // t.context.stop();
+test.after(t => {
+  t.context.stop();
   // process.exit();
 });
-*/
+
 test('It should start with no config option', async (t) => {
   // t.plan(1);
-  // const res = await request(t.context.app).get('/');
-  // t.is(200, res.status);
-  t.pass();
+  const res = await request(t.context.app).get('/');
+  t.is(200, res.status);
+  // t.pass();
 });
