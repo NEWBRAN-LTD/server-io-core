77bebe39ad65941d94f405b5777ac5b6985a0053
Joel Chu <joelckchu@gmail.com>
2019-06-25 23:11:20 +0800

    adding the code for 1.2.0

--- gulp.js
+++ gulp.js
@@ -1,2 +1,38 @@
 // This will be the gulp interface
 // const serverIoCoreGulp = require('server-io-core/gulp')
+const through = require('through2');
+const serverIoCore = require('./');
+// @TODO
+module.exports = function(options = {}) {
+  const stream = through
+    .obj((file, enc, callback) => {
+      // Serve up the files
+      app.use(config.path, serveStatic(file.path, config));
+      filePaths.push(file.path);
+      callback();
+      debug('[main][add file]', filePaths);
+    })
+    .on('data', f => {
+      filePaths.push(f.path);
+      debug('[main][data add file]', filePaths);
+    })
+    .on('end', () => {
+      debug('[main][on end]', filePaths);
+    });
+  stream.on('kill', () => {
+    // @1.4.0-beta.11 change to array
+    unwatchFn.forEach(fn => fn());
+    // Explicitly close the express server
+    server.close(() => {
+      if (io && io.server && io.server.close) {
+        // Close the socket.io server
+        io.server.close();
+      }
+    });
+    // Close the mock server
+    mockServerInstance.close();
+  });
+
+  // Return
+  return stream;
+};
--- rollup.js
+++ rollup.js
@@ -1,2 +1,37 @@
 // This will be the rollup interface
 // const serverIoCorePlugin = require('server-io-core/rollup')
+const serverIoCore = require('./');
+// @TODO
+function rollupServerIoCorePlugin(options = {}) {
+  // Force these options on config
+  const config = Object.assign(options, {
+    autoStart: false, // We must overwrite this for the plugin to work
+    reload: {
+      enable: true,
+      verbose: false
+    }
+  });
+  // Getting the fn(s)
+  const { start, stop } = serverIoCore(config);
+  // Listen to signal and close it
+  ['SIGINT', 'SIGTERM'].forEach(signal => {
+    process.on(signal, () => {
+      stop();
+      process.exit();
+    });
+  });
+  let running;
+  // Return
+  return {
+    name: 'serverIo',
+    ongenerate() {
+      if (!running) {
+        running = true;
+        start();
+      }
+    }
+  };
+}
+
+// Export
+module.exports = rollupServerIoCorePlugin;
