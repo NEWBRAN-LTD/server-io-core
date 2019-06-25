dec00d147d25999ac03d210eef71cf5c8c8fec0a
Joel Chu <joelckchu@gmail.com>
2019-06-25 19:45:19 +0800

    marking the 1.2.0 development

---
+++ gulp.js
@@ -0,0 +1,2 @@
+// This will be the gulp interface
+// const serverIoCoreGulp = require('server-io-core/gulp')
---
+++ rollup.js
@@ -0,0 +1,2 @@
+// This will be the rollup interface
+// const serverIoCorePlugin = require('server-io-core/rollup')
