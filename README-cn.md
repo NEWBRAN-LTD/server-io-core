3aa0011de38386f88bf860b6c54f1d8871723d12
Joel Chu <joelckchu@gmail.com>
2019-06-30 11:38:13 +0800

    preparing for v1.3.0 development

--- README-cn.md
+++ README-cn.md
@@ -2,8 +2,6 @@
 
 > 基于 Koa 的开发服务器, 具有内置的重新加载、观察程序、远程调试器、代理服务器和其他一些功能.
 
-这是我 [gulp-freeal-io](http://gitlab. com/newbrantd/gulp-fergop-eio) 的一个分支 (如果你觉得有用, 请给它一个明星, 谢谢)
-
 ## 安装
 
 在脚本中使用此功能
---
+++ src/lib/server/proxy-server.js
@@ -0,0 +1,4 @@
+// For v1.3.x development to replace all the DIY proxy options back to node-http-proxy
+// The idea is actually very simple
+// we start up the proxy server when we start our website and hijack the call before
+// it reaches Koa
