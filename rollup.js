75746c8d916b79a54d60b734d4c45862114fb559
Joel Chu <joelckchu@gmail.com>
2022-06-22 23:24:46 +0800

    finalize on the rollup file

--- rollup.js
+++ rollup.js
@@ -3,6 +3,10 @@
 import serverIoCore from './index.mjs'
 // @TODO
 export default function rollupServerIoCorePlugin (options = {}) {
+  // if no port specified, we set it to random
+  if (!('port' in options)) {
+    options.port = 0
+  }
   // Force these options on config
   const config = Object.assign(options, {
     autoStart: false, // We must overwrite this for the plugin to work
