eb5259d60a4848c15e84ce1bda259ea932ea6a9a
Joel Chu <joelckchu@gmail.com>
2022-06-23 07:17:21 +0800

    unable to make that warning go away

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,2 +1,40 @@
-// This is for testing our own rollup plugin
-import rollupServerIoCorePlugin from './rollup'
+// Building a cjs version for compatibility reason
+import { join } from 'node:path'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json'
+import buble from '@rollup/plugin-buble'
+import { getDirname } from './src/utils/index.mjs'
+const __dirname = getDirname(import.meta.url)
+
+const plugins = [
+  commonjs(),
+  nodeResolve({ preferBuiltins: true }),
+  json()
+]
+const outplugins = [
+  buble()
+]
+
+export default [{
+  external: /node_modules/,
+  input: join(__dirname, 'index.mjs'),
+  output: [{
+    exports: 'default',
+    format: 'cjs',
+    file: join(__dirname, 'index.js'),
+    sourcemap: true,
+    plugins: outplugins
+  }],
+  plugins
+}, {
+  external: /node_modules/,
+  input: join(__dirname, 'cli.mjs'),
+  output: [{
+    format: 'cjs',
+    file: join(__dirname, 'cli.js'),
+    sourcemap: false,
+    plugins: outplugins
+  }],
+  plugins
+}]
