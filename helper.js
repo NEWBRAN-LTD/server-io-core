57a7dd291654686a76f70f776a2513b70c2b8e27
Joel Chu <joelckchu@gmail.com>
2022-06-23 13:49:35 +0800

    move more of the qunit methods over to the helpers

--- helper.js
+++ helper.js
@@ -2,6 +2,7 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
+var node_path = require('node:path');
 var glob = require('glob');
 
 function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
@@ -21,4 +22,39 @@ async function searchFiles (dest) {
   })
 }
 
-exports.searchFiles = searchFiles;
+// just grab the middlewares if any
+const getMiddlewares = (config) => {
+  const { middlewares } = config;
+  if (middlewares) {
+    return Array.isArray(middlewares) ? middlewares : [middlewares]
+  }
+  return []
+};
+
+/**
+ * @param {object} config configuration
+ * @return {object} promise resolve the config for server-io-core
+ */
+const getConfigForQunit = (config) => {
+  const baseDir = node_path.resolve(node_path.join(config.baseDir, 'qunit', 'files'));
+  return searchFiles(node_path.join(baseDir, config.testFilePattern))
+    .then(files => (
+      {
+        qunit: true, // MUST SET TO TRUE
+        port: config.port,
+        webroot: config.webroot,
+        open: config.open,
+        reload: config.reload,
+        middlewares: getMiddlewares(config),
+        // DON"T TOUCH THIS UNLESS YOU KNOW WHAT YOU ARE DOING //
+        inject: {
+          insertBefore: false,
+          target: {
+            body: files.map(file => file.replace(baseDir, ''))
+          }
+        }
+      }
+    ))
+};
+
+exports.getConfigForQunit = getConfigForQunit;
--- helper.mjs
+++ helper.mjs
@@ -1,7 +1,8 @@
 // export extra methods to help with other things
+import { join, resolve } from 'node:path'
 import glob from 'glob'
 /** just a wrapper of glob to make it async */
-export async function searchFiles (dest) {
+async function searchFiles (dest) {
   return new Promise((resolve, reject) => {
     glob(dest, function (err, files) {
       if (err || !files.length) {
@@ -10,4 +11,39 @@ export async function searchFiles (dest) {
       resolve(files)
     })
   })
+}
+
+// just grab the middlewares if any
+const getMiddlewares = (config) => {
+  const { middlewares } = config
+  if (middlewares) {
+    return Array.isArray(middlewares) ? middlewares : [middlewares]
+  }
+  return []
+}
+
+/**
+ * @param {object} config configuration
+ * @return {object} promise resolve the config for server-io-core
+ */
+export const getConfigForQunit = (config) => {
+  const baseDir = resolve(join(config.baseDir, 'qunit', 'files'))
+  return searchFiles(join(baseDir, config.testFilePattern))
+    .then(files => (
+      {
+        qunit: true, // MUST SET TO TRUE
+        port: config.port,
+        webroot: config.webroot,
+        open: config.open,
+        reload: config.reload,
+        middlewares: getMiddlewares(config),
+        // DON"T TOUCH THIS UNLESS YOU KNOW WHAT YOU ARE DOING //
+        inject: {
+          insertBefore: false,
+          target: {
+            body: files.map(file => file.replace(baseDir, ''))
+          }
+        }
+      }
+    ))
 }
