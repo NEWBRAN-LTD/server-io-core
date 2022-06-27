d7b771441d2bceb8deee338dd6a91f2de5aff1e5
Joel Chu <joelckchu@gmail.com>
2022-06-27 20:25:19 +0800

    puting the shebag back into the cli

--- cli.js
+++ cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // cjs version of the cli
 const { serve } = require('./cjs/lib/cli.mjs')
 const { socketIoCore } = require('./cjs/index.mjs')
--- cli.mjs
+++ cli.mjs
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // Cli interface in ESM
 import { serve } from './src/lib/cli.mjs'
 import { serverIoCore } from './src/index.mjs'
