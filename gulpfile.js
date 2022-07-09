7a5a7aebd4a89f18458fb290d48b0c0931ec8f2c
Joel Chu <joelckchu@gmail.com>
2022-07-09 10:20:12 +0800

    remove all the unused old files

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-// For testing purposes
-const gulp = require('gulp')
-const serverIoCore = require('./gulp')
-
-gulp.task('default', async () =>
-  gulp.src(['./__tests__/fixtures/demo/dist/base']).pipe(serverIoCore())
-)
